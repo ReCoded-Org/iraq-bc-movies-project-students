3c3e2016163f70523d38632064d9bebe003d80be
Qays <work.worker55@gmail.com>
2020-06-22 19:32:07 +0300

    adding bootstrap

--- hanan_shirin_qays/moivesapp/src/App.js
+++ hanan_shirin_qays/moivesapp/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
