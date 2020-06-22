04af883c09d25798c4a39db4a92dd4e3bf570d17
Hanan <ensw03217@student.su.edu.krd>
2020-06-23 00:06:59 +0300

    Version 1

--- hanan_shirin_qays/moivesapp/src/App.js
+++ hanan_shirin_qays/moivesapp/src/App.js
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import Header from "./Components/Header";
+import Main from "./Components/Main";
+import Footer from "./Components/Footer";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page-container">
+      <div className="content-wrap">
+        <Header />
+        <Main />
+      </div>
+      <Footer />
     </div>
   );
 }
