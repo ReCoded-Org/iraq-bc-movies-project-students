664fe86d5af4e0a061c30e943e56aae9c5c41b0a
Mahmoud Mansour <mahmodlte@gmail.com>
2020-06-24 09:50:24 +0300

    first version of navbar component

--- react-assissans/src/index.js
+++ react-assissans/src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import Navbar from "./components/navbar";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 ReactDOM.render(
   <React.StrictMode>
     <Navbar />
