43c6bd61bd03c87b515d395b0631992051f78207
Mahmoud Mansour <mahmodlte@gmail.com>
2020-06-25 22:09:39 +0300

    Spinner done

--- react-assissans/src/components/navbar.js
+++ react-assissans/src/components/navbar.js
@@ -1,6 +1,7 @@
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, Spinner } from "react-bootstrap";
 import React, { useState } from "react";
-import Spinner from "./Spinner";
+const moviesUrl =
+  "https://api.themoviedb.org/3/search/movie/?api_key=1d54e327869a62aba4dc1b58c2b30233";
 
 function navBar() {
   return (
@@ -10,65 +11,64 @@ function navBar() {
         <Nav>
           <Nav.Link href="#home">Home</Nav.Link>
         </Nav>
-        <Spinner />
         <SearchBox />
       </Navbar>
     </>
   );
 }
-//function onSearch(props) {
-// const []
-//}
-const Search = () => {
-  const [showResults, setShowResults] = React.useState(false);
-  const onClick = () => setShowResults(true);
-  return (
-    <div>
-      <input type="submit" value="Search" onClick={onClick} />
-      {showResults ? <Results /> : null}
-    </div>
-  );
-};
 
-function SearchBox(props) {
+function SearchBox() {
   const [value, setValue] = useState("");
+  let [loading, setLoading] = useState(false);
 
   const handleChange = (event) => {
+    setLoading(true);
+
     setValue(event.target.value);
-    console.log(moviesData(value));
+    fetch(`${moviesUrl}&query=${value}`)
+      .then((movies) => movies.json())
+      .then((movieData) => {
+        setLoading(false);
+        console.log(movieData.results);
+      });
   };
   const handleSubmit = (event) => {
+    setLoading(true);
+    fetch(`${moviesUrl}&query=${value}`)
+      .then((movies) => movies.json())
+      .then((movieData) => {
+        setLoading(false);
+        console.log(movieData.results);
+      });
     event.preventDefault();
-    moviesData(value);
   };
   return (
-    <form className="form-inline my-2" onSubmit={handleSubmit}>
-      <input
-        className="form-control"
-        type="search"
-        placeholder="Search for a Movie"
-        value={value}
-        onChange={handleChange}
-      ></input>
-      <button className=" btn btn-primary ml-3" type="submit">
-        Search
-      </button>
-    </form>
+    <>
+      <form className="form-inline my-2" onSubmit={handleSubmit}>
+        <input
+          className="form-control"
+          type="search"
+          placeholder="Search for a Movie"
+          value={value}
+          onChange={handleChange}
+        ></input>
+        <button className=" btn btn-primary ml-3" type="submit">
+          Search
+        </button>
+      </form>
+      {loading && <Loading />}
+    </>
   );
 }
-const spinnr = (props) => {
+//function OnSearch() {
+//return <Loading value="inline-block" />;
+//}
+const Loading = (props) => {
+  console.log(props.value);
   return (
-    <Spinner style={{ display: "none" }} animation="border" role="status">
-      <span className="sr-only spin">Loading...</span>
+    <Spinner animation="grow" role="status" variant="warning" className="ml-4">
+      <span className="sr-only spin ">Loading...</span>
     </Spinner>
   );
 };
 export default navBar;
-
-function moviesData(Searchquery) {
-  fetch(
-    `https://api.themoviedb.org/3/search/movie/?api_key=1d54e327869a62aba4dc1b58c2b30233&query=${Searchquery}`
-  )
-    .then((movies) => movies.json())
-    .then((movieData) => console.log(movieData.results));
-}
