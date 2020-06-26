c8be4e92d818d4aa1d94f15e54bcb3c7f87ad93a
Shirin Muataz <shirinmuataz@gmail.com>
2020-06-26 12:52:28 +0300

    Done with request 32 + fixing some bugs - shirin

--- hanan_shirin_qays/moivesapp/src/Components/Main.js
+++ hanan_shirin_qays/moivesapp/src/Components/Main.js
@@ -1,17 +1,19 @@
 import React, { useState, useEffect } from "react";
-import RenderMovie from "./RenderMovie";
-import {constructUrl} from './Api';
+//import RenderMovie from "./RenderMovie";
+//import { Form } from "react-bootstrap";
+import { constructUrl } from './Api';
+import MovieItem from './MovieItem';
 function Main(props) {
   const query = props.query;
   console.log(query);
-  
 
-  const SEARCH_URL = constructUrl('search/movie',query);
- 
- 
+
+  const SEARCH_URL = constructUrl('search/movie', query);
+
+
 
   const [movies, setMovies] = useState([]);
-  
+
   useEffect(() => {
 
     fetch(SEARCH_URL)
@@ -19,16 +21,18 @@ function Main(props) {
       .then((data) => {
         console.log(data);
         if (data.results != undefined)
-        setMovies(data.results);
-      }).catch(err=> console.log(err));
-    },[query]);
-  
+          setMovies(data.results);
+      }).catch(err => console.log(err));
+  }, [query]);
+
   console.log(movies);
-  
+  MovieItem(movies);
   return (
     <React.Fragment>
-      {movies.length > 0 && movies.map(movie=><h1>{movie.title}</h1>)}
+      {movies.length > 0 && movies.map(movie => <h1 key={movie.id}>{movie.title}</h1>)}
     </React.Fragment>
   );
+
 }
+
 export default Main;
---
+++ hanan_shirin_qays/moivesapp/src/Components/MovieItem.js
@@ -0,0 +1,7 @@
+import React, { Component, useState } from 'react';
+
+export default function MovieItem(props) {
+    let movieList = [];
+    movieList = props.map(movie => movie.title);
+    console.log("This is Mapped Movie Grid" + `\n` + movieList);
+}
