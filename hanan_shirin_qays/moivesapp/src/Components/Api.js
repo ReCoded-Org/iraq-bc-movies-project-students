80a88a46433899d177c8aaaf3ee2500783024c9d
Hanan <ensw03217@student.su.edu.krd>
2020-06-26 16:08:32 +0300

    adding categories

--- hanan_shirin_qays/moivesapp/src/Components/Api.js
+++ hanan_shirin_qays/moivesapp/src/Components/Api.js
@@ -1,6 +1,3 @@
-
-
-
 const TMDB_BASE_URL = `https://api.themoviedb.org/3`;
 const API_KEY = "542003918769df50083a13c415bbc602";
 // const SEARCH_URL = TMDB_BASE_URL + `search/movie?${API_KEY}&query=${query}`;
@@ -10,8 +7,7 @@ const API_KEY = "542003918769df50083a13c415bbc602";
 //   return `${TMDB_BASE_URL}/${path}?api_key=542003918769df50083a13c415bbc602&query=${query}`;
 // };
 
-export function constructUrl (path, query) {
-  return `${TMDB_BASE_URL}/${path}?api_key=${API_KEY}&query=${query}`;};
-
-
-  
+export function constructUrl(path, query) {
+  const queryText = query ? `&query=${query}` : "";
+  return `${TMDB_BASE_URL}/${path}?api_key=${API_KEY}` + queryText;
+}
--- hanan_shirin_qays/moivesapp/src/Components/Main.js
+++ hanan_shirin_qays/moivesapp/src/Components/Main.js
@@ -1,34 +1,60 @@
 import React, { useState, useEffect } from "react";
 import RenderMovie from "./RenderMovie";
-import {constructUrl} from './Api';
+import { constructUrl } from "./Api";
+import { Row, Col } from "react-simple-flex-grid";
+import { Navbar, Nav } from "react-bootstrap";
+
 function Main(props) {
-  const query = props.query;
-  console.log(query);
-  
-
-  const SEARCH_URL = constructUrl('search/movie',query);
- 
- 
-
   const [movies, setMovies] = useState([]);
-  
+  const query = props.query;
+  const categorie = props.categorie;
   useEffect(() => {
-
+    let SEARCH_URL;
+    if (query !== "") {
+      SEARCH_URL = constructUrl("search/movie", query);
+    } else {
+      SEARCH_URL = constructUrl("movie/popular");
+    }
     fetch(SEARCH_URL)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
-        if (data.results != undefined)
-        setMovies(data.results);
-      }).catch(err=> console.log(err));
-    },[query]);
-  
-  console.log(movies);
-  
+        if (data.results !== undefined) {
+          let movies = data.results;
+          console.log(movies);
+          console.log(categorie);
+
+          if (categorie.id) {
+            movies = movies.filter((movie) =>
+              movie.genre_ids.includes(categorie.id)
+            );
+            console.log(movies);
+          }
+          props.setIsSpinnerHidden(true);
+          setMovies(movies);
+        }
+      })
+      .catch((err) => console.log(err));
+  }, [query, categorie]);
+
   return (
-    <React.Fragment>
-      {movies.length > 0 && movies.map(movie=><h1>{movie.title}</h1>)}
-    </React.Fragment>
+    <>
+      <Row gutter={40}>
+        {movies.length > 0 &&
+          movies.map((movie) => (
+            <Col span={6} align="middle">
+              <RenderMovie
+                overview={movie.overview}
+                src={
+                  "https://image.tmdb.org/t/p/original" + movie.backdrop_path
+                }
+                key={movie.id}
+                title={movie.title}
+              />
+            </Col>
+          ))}
+      </Row>
+    </>
   );
 }
 export default Main;
