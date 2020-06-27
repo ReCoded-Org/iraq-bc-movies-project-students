f0881b048e28c07c97ce555abf2375a5dcba6429
Ahmad A. Duhoki <ahmad.a.duhoki99@gmail.com>
2020-06-27 18:31:42 +0300

    add app.js

--- react-assissans/src/app.js
+++ react-assissans/src/app.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/navbar";
 import Main from "./components/main";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState("");
+
+  const handleMovies = (moviesResult) => {
+    return setMovies([moviesResult]);
+  };
+
   return (
-    <div >
-    <Navbar />
-    <Main />
+    <div>
+      <Navbar handleMovies={handleMovies} />
+      <Main query={query} />
     </div>
   );
 }
