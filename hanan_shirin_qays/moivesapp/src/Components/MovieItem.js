567be9f1b80e82f52854c96baef2306127ba9c6d
Shirin Muataz <shirinmuataz@gmail.com>
2020-06-27 03:37:13 +0300

    got back after messing up

--- hanan_shirin_qays/moivesapp/src/Components/MovieItem.js
+++ hanan_shirin_qays/moivesapp/src/Components/MovieItem.js
@@ -1,25 +1,28 @@
-
-import React, { useState } from "react";
-import Card from 'react-bootstrap/Card'
+import React, { component } from 'react';
+//import ReactHover from 'react - hover';
+//import styled from "@emotion/styled/macro";
+//import { useState } from "react";
+import Card from 'react-bootstrap/Card';
 //import { Col } from "react-bootstrap";
 
 export default function MovieItem(props) {
+    //const [isShown, setIsShown] = useState(false);
     //console.log(props.movie.backdrop_path);
     const baseUrl = "https://image.tmdb.org/t/p/w500/";
-    return (<>
-        <Card>
-            <Card.Img variant="top" src={baseUrl + props.backdrop_path} />
+    const nullPhoto = "https://image.tmdb.org/t/p/w500/wwemzKWzjKYJFfCeiB57q3r4Bcm.png";
+    return (<div>
+        <Card.Img variant="top" src={props.backdrop_path !== null ? baseUrl + props.backdrop_path : nullPhoto} />
+        <Card.Body>
+            <Card.Text>
+                {props.title}
+            </Card.Text>
+        </Card.Body>
 
-            <Card.Body>
-                <Card.Text>
-                    {props.title}
-                </Card.Text>
-            </Card.Body>
-        </Card>
-    </>);
+    </div>);
     //<Col lg={true}><img src=></img><p></p></Col >);
     //<Col xs>Second, but unordered</Col>
     //<Col xs={{ order: 'first' }}>Third, but first</Col>
+
 
 
 
