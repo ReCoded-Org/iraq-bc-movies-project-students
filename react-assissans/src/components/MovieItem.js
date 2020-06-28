e3de4730705c723ca98fac514b36d39ef6b88c67
Ahmad A. Duhoki <ahmad.a.duhoki99@gmail.com>
2020-06-28 23:25:39 +0300

    fixed the image show of the grids

--- react-assissans/src/components/MovieItem.js
+++ react-assissans/src/components/MovieItem.js
@@ -3,11 +3,12 @@ import { Card, Badge } from "react-bootstrap";
 
 function MovieItem(props) {
   const { item } = props;
+  const img = `https://image.tmdb.org/t/p/w500${item.poster_path}`;
   console.log(item);
   return (
     <div>
       <Card style={{ width: "20rem", hieght: "10rem" }}>
-        <Card.Img variant="top" src={item.poster_path} />
+        <Card.Img variant="top" src={img} />
         <Card.Body>
           <Card.Title>{item.title}</Card.Title>
           <Card.Text>{item.overview}</Card.Text>
