2b0f13619f9982f786f1fc6f01e37e232a388b9b
Qays <work.worker55@gmail.com>
2020-06-28 22:08:22 +0300

    Finsinging adding stars

--- hanan_shirin_qays/moivesapp/src/Components/MovieItem.js
+++ hanan_shirin_qays/moivesapp/src/Components/MovieItem.js
@@ -8,39 +8,28 @@ export default function MovieItem(props) {
     "https://image.tmdb.org/t/p/w500/wwemzKWzjKYJFfCeiB57q3r4Bcm.png";
   return (
     <Card key={props.id} style={{ width: "50%" }}>
-      <div class="card image">
-        <Card.Img
-          src={
-            props.backdrop_path !== null
-              ? baseUrl + props.backdrop_path
-              : nullPhoto
-          }
-        />
-        <React.Fragment>
-          <h6 class="center datails">
-            {props.title}
-            <br />
-            <span>Vote Average:
-            
-              
-              <ReactStars
+        <div class="card image">
+      <Card.Img src={props.backdrop_path !== null
+        ? baseUrl + props.backdrop_path
+        : nullPhoto} />
+      <React.Fragment >
+        <h6 class="center datails">{props.title}<br /><span>Vote Average: <ReactStars
                 count={10}
                 value={props.vote_average}
-                half={true}
                 size={14}
-                color2={"#ffd700"}/>
-             ({props.vote_average})</span>
-          </h6>
-        </React.Fragment>
-        <div class="details">
-          <div class="center">
-            <p>
-              Released <br />
-              {props.release_date}
-            </p>
-          </div>
+                color2={"#ffd700"}/> ({props.vote_average})</span></h6>
+      </React.Fragment>
+      <div class="details">
+        <div class="center">
+          <p>Released <br />{props.release_date}</p>
         </div>
       </div>
+    </div>
+
+
     </Card>
+
+
+   
   );
 }
