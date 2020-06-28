bae5c8a1703a2fac376a18f320e75c51353e8801
Shirin Muataz <shirinmuataz@gmail.com>
2020-06-28 06:21:48 +0300

    done- with hover

--- hanan_shirin_qays/moivesapp/src/Components/MovieItem.js
+++ hanan_shirin_qays/moivesapp/src/Components/MovieItem.js
@@ -5,25 +5,21 @@ export default function MovieItem(props) {
   const baseUrl = "https://image.tmdb.org/t/p/w500/";
   const nullPhoto =
     "https://image.tmdb.org/t/p/w500/wwemzKWzjKYJFfCeiB57q3r4Bcm.png";
-  return (<><Card className="mt-4 mb-4">
+  return (
+    <Card className="mt-4 mb-4">
 
-    <div class="card image">
-      <Card.Img src={props.backdrop_path !== null
-        ? baseUrl + props.backdrop_path
-        : nullPhoto} />
-
-      <React.Fragment >
-        <h6 class="center datails">{props.title}<br /><span>Vote Average: {props.vote_average}</span></h6>
-
-
-      </React.Fragment>
-
-      <div class="details">
-        <div class="center">
-          <p>Released <br />{props.release_date}</p>
+      <div class="card image">
+        <Card.Img src={props.backdrop_path !== null
+          ? baseUrl + props.backdrop_path
+          : nullPhoto} />
+        <React.Fragment >
+          <h6 class="center datails">{props.title}<br /><span>Vote Average: {props.vote_average}</span></h6>
+        </React.Fragment>
+        <div class="details">
+          <div class="center">
+            <p>Released <br />{props.release_date}</p>
+          </div>
         </div>
       </div>
-    </div>
-  </Card></>);
-
+    </Card>);
 }
