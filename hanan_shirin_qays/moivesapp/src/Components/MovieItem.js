3c47e5b39bbbbb6435ea751acfccabd1770f4f2b
Qays <work.worker55@gmail.com>
2020-06-28 21:34:15 +0300

    Adding stars as rating

--- hanan_shirin_qays/moivesapp/src/Components/MovieItem.js
+++ hanan_shirin_qays/moivesapp/src/Components/MovieItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Card, { CardBody } from "react-bootstrap/Card";
+import ReactStars from "react-rating-stars-component";
 
 export default function MovieItem(props) {
   const baseUrl = "https://image.tmdb.org/t/p/w500/";
@@ -7,19 +8,39 @@ export default function MovieItem(props) {
     "https://image.tmdb.org/t/p/w500/wwemzKWzjKYJFfCeiB57q3r4Bcm.png";
   return (
     <Card className="mt-4 mb-4">
-
       <div class="card image">
-        <Card.Img src={props.backdrop_path !== null
-          ? baseUrl + props.backdrop_path
-          : nullPhoto} />
-        <React.Fragment >
-          <h6 class="center datails">{props.title}<br /><span>Vote Average: {props.vote_average}</span></h6>
+        <Card.Img
+          src={
+            props.backdrop_path !== null
+              ? baseUrl + props.backdrop_path
+              : nullPhoto
+          }
+        />
+        <React.Fragment>
+          <h6 class="center datails">
+            {props.title}
+            <br />
+            <span>Vote Average:
+            
+              
+              <ReactStars
+                count={10}
+                value={props.vote_average}
+                half={true}
+                size={14}
+                color2={"#ffd700"}/>
+             ({props.vote_average})</span>
+          </h6>
         </React.Fragment>
         <div class="details">
           <div class="center">
-            <p>Released <br />{props.release_date}</p>
+            <p>
+              Released <br />
+              {props.release_date}
+            </p>
           </div>
         </div>
       </div>
-    </Card>);
+    </Card>
+  );
 }
