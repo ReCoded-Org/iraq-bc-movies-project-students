aa07fb3605d16ff6b823974eada2bb684c9af974
Shirin Muataz <shirinmuataz@gmail.com>
2020-06-28 06:15:39 +0300

    Setting MovieItem Hover effect

--- hanan_shirin_qays/moivesapp/src/Components/MoviesGrid.js
+++ hanan_shirin_qays/moivesapp/src/Components/MoviesGrid.js
@@ -6,7 +6,7 @@ import Row from "react-bootstrap/Row";
 export default function MoviesGrid(props) {
   return (
     <Container>
-      <Row md={4} lg={4} xs={12}>{props.movies.map((movie) => MovieItem(movie))}</Row>
+      <Row md={4} lg={3} xs={12}>{props.movies.map((movie) => MovieItem(movie))}</Row>
     </Container>
   );
 }
