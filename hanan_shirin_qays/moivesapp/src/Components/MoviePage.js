62ecda330673b9f2b5666a8cf46dead291faf664
Qays <work.worker55@gmail.com>
2020-06-30 18:30:17 +0300

    Adding notes

--- hanan_shirin_qays/moivesapp/src/Components/MoviePage.js
+++ hanan_shirin_qays/moivesapp/src/Components/MoviePage.js
@@ -28,6 +28,22 @@ console.log(movie_id);
     let data = await res.json();
     setMovie(data);
   }, [movie_id]);
+  const baseUrl = "https://image.tmdb.org/t/p/w500/";
+  const nullPhoto =
+    "https://image.tmdb.org/t/p/w500/wwemzKWzjKYJFfCeiB57q3r4Bcm.png";
+
+
+
+
+
+//     Genres (you will need to fetch all the genres and do some comparison do get the name of the genres)
+// you fetch them same as the drop down nav thing and 
+
+// Trailers of this movie (which will include another fetch)
+// you use ifram to show the youtube embeded
+// Actors of this movie (which will include another fetch)
+// you fetch each actor from his id from the data base and display there names 
+//you use alot of for loop and async await 
 
   return (
     <>
@@ -37,7 +53,17 @@ console.log(movie_id);
         </a>
       </Button>
 
+
       {movie.original_title}
+
+
+      <img
+          src={
+            movie.backdrop_path !== null
+              ? baseUrl + movie.backdrop_path
+              : nullPhoto
+          }
+          />
     </>
   );
 }
