e54411a4184f23212d910c2f56f3c5f3bda9fd4b
Qays <work.worker55@gmail.com>
2020-06-30 17:36:57 +0300

    confliect

--- hanan_shirin_qays/moivesapp/src/Components/MainPage.js
+++ hanan_shirin_qays/moivesapp/src/Components/MainPage.js
@@ -3,30 +3,13 @@ import MoviesGrid from "./MoviesGrid";
 import Navbar from "./Navbar";
 
 function MainPage(props) {
-  const [isLoading, setIsLoading] = useState(true);
-  const [query, setQuery] = useState("");
-  const [movies, setMovies] = useState([]);
-  const [movieId, setMovieId] = useState("");
-
-  const handleQuery = (query) => {
-    setQuery(query);
-  };
-
-  const handleMovies = (movies) => {
-    setMovies(movies);
-    setIsLoading(false);
-  };
+ 
 
   return (
     <React.Fragment>
-      <Navbar
-        handleQuery={handleQuery}
-        handleMovies={handleMovies}
-        isLoading={isLoading}
-        setIsLoading={setIsLoading}
-      />
+  
       {movies.length > 0 && (
-        <MoviesGrid movies={movies} setMovieId={setMovieId} />
+        <MoviesGrid movies={props.movies} setMovieId={props.setMovieId} />
       )}
     </React.Fragment>
   );
