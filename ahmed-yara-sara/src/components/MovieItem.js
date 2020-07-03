1f63537392a17b821956972d9da7b9c998eccb69
Yara Alkassar <yara.sarmad@yahoo.com>
2020-07-03 10:08:25 +0300

    removed some unused code

--- ahmed-yara-sara/src/components/MovieItem.js
+++ ahmed-yara-sara/src/components/MovieItem.js
@@ -2,34 +2,33 @@ import React, { useState } from "react";
 import { Card, Col } from "react-bootstrap";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 function MovieItem(props) {
-  const { title, overview, popularity, release_date } = props;
+  const { title, popularity, release_date } = props;
   const [select, onSelect] = useState("");
   function setMovieId() {
     onSelect(props.id);
-    console.log(props.id);
   }
   return (
     <>
-      <a
-        href={`Movie/${props.id}`}
-        style={{ textDecoration: "none" }}
-        className="d-flex justify-content-center"
-      >
-        <ul style={{ listStyleType: "none" }}>
-          <Col>
+      <div className="d-flex justify-content-center mx-auto my-3">
+        <Col>
+          <a
+            href={`Movie/${props.id}`}
+            style={{ textDecoration: "none" }}
+            className="d-flex justify-content-center"
+          >
             <Card
               className="m-auto p-2"
               style={{ width: "400px", color: "black" }}
-              onClick={setMovieId}
             >
               <app setMovieId={setMovieId}></app>
               <Card.Img
                 variant="top"
                 src={`https://image.tmdb.org/t/p/w500/${props.image}`}
               />
+
               <Card.Body>
                 <div>
-                  <li>{title}</li>
+                  <p>{title}</p>
                 </div>
                 <div className="d-flex justify-content-center pt-2">
                   <div className="m-auto p-2">
@@ -45,9 +44,9 @@ function MovieItem(props) {
                 </div>
               </Card.Body>
             </Card>
-          </Col>
-        </ul>
-      </a>
+          </a>
+        </Col>
+      </div>
     </>
   );
 }
