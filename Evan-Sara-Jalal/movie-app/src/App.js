2fb6ad016ccb30ea37f615ec2d30878758745df6
evan hameed <evanhameed9966@gmail.com>
2020-07-04 11:40:52 +0300

    Redirecting on npm start

--- Evan-Sara-Jalal/movie-app/src/App.js
+++ Evan-Sara-Jalal/movie-app/src/App.js
@@ -7,15 +7,20 @@ import ActorInfo from "./components/ActorInfo";
 import Footer from "./components/Footer";
 import { useState, useEffect } from "react";
 import MoviePage from "./components/moviePage";
-import { BrowserRouter as Router, Switch, Route,Redirect } from "react-router-dom";
-import  {useHistory} from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 function App() {
   const [movieList, setMovieList] = useState([]);
   const [searchedTerm, setSearchedTerm] = useState("");
   const [genreList, setGenreList] = useState([]);
   const [genreId, setGenreId] = useState(28);
-  const history = useHistory()
+  const history = useHistory();
 
   const TMDB_BASE_URL = "https://api.themoviedb.org/3";
   const constructUrl = (query) => {
@@ -27,7 +32,7 @@ function App() {
   useEffect(() => {
     fetchGenres();
     fetchPopularMovies();
-  },  [genreId]);
+  }, [genreId]);
 
   const fetchGenres = () => {
     fetch(
@@ -39,16 +44,18 @@ function App() {
       });
   };
 
-  const fetchPopularMovies= ()=>{
+  const fetchPopularMovies = () => {
     fetch(
       `https://api.themoviedb.org/3/movie/popular?api_key=7a757097cf4698b75ed2848485f49571&language=en-US&page=1`
     )
-      .then(resp => resp.json())
-      .then(resp => {
-        const filtered = resp.results.filter(movie=> movie.genre_ids.includes(parseInt(genreId)))
-        setMovieList(filtered)
+      .then((resp) => resp.json())
+      .then((resp) => {
+        const filtered = resp.results.filter((movie) =>
+          movie.genre_ids.includes(parseInt(genreId))
+        );
+        setMovieList(filtered);
       });
-  }
+  };
 
   function handleGenreChange(evt) {
     setGenreId(evt.target.value);
@@ -56,7 +63,6 @@ function App() {
     console.log(genreId);
   }
   function handleSubmit(e) {
-
     fetch(constructUrl(searchedTerm))
       .then((data) => data.json())
       .then((data) => {
@@ -65,11 +71,7 @@ function App() {
           movie.genre_ids.includes(parseInt(genreId))
         );
         setMovieList(filtered);
-       
-      
-      })
-
-
+      });
   }
 
   function handleChange(e) {
@@ -88,6 +90,7 @@ function App() {
           name="Netplex "
         />
         <div className="main-wrapper">
+          <Redirect to="/" />
           <Switch>
             <Route path="/" exact>
               {" "}
@@ -95,8 +98,11 @@ function App() {
             </Route>
             <Route path="/:id" exact component={MoviePage} />
             <Route path="/person/:actor_id" component={ActorInfo} />
-            <Route render={()=>{return <h1>Page not found (404)</h1>}} />
-            <Redirect to=''/>
+            <Route
+              render={() => {
+                return <h1>Page not found (404)</h1>;
+              }}
+            />
           </Switch>
         </div>
         <Footer />
