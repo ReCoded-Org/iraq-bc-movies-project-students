e86b14d3812f4579e9150b0d3f2a5ab69208901d
Qays <work.worker55@gmail.com>
2020-07-04 17:34:48 +0300

    Implimation stateprovider

--- hanan_shirin_qays/moivesapp/src/App.js
+++ hanan_shirin_qays/moivesapp/src/App.js
@@ -6,6 +6,8 @@ import MainPage from "./Components/MainPage";
 import MoviePage from "./Components/MoviePage";
 import Footer from "./Components/Footer";
 import ActorPage from "./Components/ActorPage";
+import {StateProvider} from "./Components/StateProvider";
+
 import {
   BrowserRouter as Router,
   Switch,
@@ -15,38 +17,39 @@ import {
 } from "react-router-dom";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [query, setQuery] = useState("");
-  const [movies, setMovies] = useState([]);
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [query, setQuery] = useState("");
+  // const [movies, setMovies] = useState([]);
 
-  const handleQuery = (query) => {
-    setQuery(query);
-  };
+  // const handleQuery = (query) => {
+  //   setQuery(query);
+  // };
 
-  const handleMovies = (movies) => {
-    setMovies(movies);
-    setIsLoading(false);
-  };
+  // const handleMovies = (movies) => {
+  //   setMovies(movies);
+  //   setIsLoading(false);
+  // };
 
   return (
+    <StateProvider>
     <div className="page-container">
       <div className="content-wrap">
         <Navbar
-          handleQuery={handleQuery}
-          handleMovies={handleMovies}
-          isLoading={isLoading}
-          setIsLoading={setIsLoading}
+          // handleQuery={handleQuery}
+          // handleMovies={handleMovies}
+          // isLoading={isLoading}
+          // setIsLoading={setIsLoading}
         />
         <HashRouter>
           <Switch>
-            <Route exact path="/">
-              <MainPage
+            <Route exact path="/"component={MainPage} />
+              {/* <MainPage
                 isLoading={isLoading}
                 setIsLoading={setIsLoading}
                 movies={movies}
                 query={query}
               />
-            </Route>
+            </Route> */}
             <Route
               path="/Movie/:id"
               component={MoviePage}
@@ -57,6 +60,7 @@ function App() {
       </div>
       <Footer />
     </div>
+    </StateProvider>
   );
 }
 export default App;
