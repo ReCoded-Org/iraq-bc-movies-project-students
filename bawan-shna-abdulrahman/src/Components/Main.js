b21365f90f0515d3cb579532fa4d8a3561d428be
Mahmoud Mansour <mahmodlte@gmail.com>
2020-07-08 14:02:43 +0300

    reomved other files to avoid conflect

--- bawan-shna-abdulrahman/src/Components/Main.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import MovieGrid from "./MovieGrid";
-
-export default function Main(props) {
-  const { movies } = props;
-  return <>{movies.length > 0 && <MovieGrid movies={movies} />}</>;
-}
